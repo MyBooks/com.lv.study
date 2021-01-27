b0107a49c5dc0a69eb3db2bd049dc423e188d845
MyBooks <1154712196@qq.com>
2021-01-27 08:20:24 +0800

    eureka新增单机配置，gateway和flux注册eureka

--- flux/src/main/java/com/lv/study/flux/FluxApplication.java
+++ flux/src/main/java/com/lv/study/flux/FluxApplication.java
@@ -3,6 +3,7 @@ package com.lv.study.flux;
 import com.lv.study.flux.controller.Demo2Controller;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
@@ -12,6 +13,7 @@ import org.springframework.web.reactive.function.server.ServerResponse;
 import java.security.PublicKey;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class FluxApplication {
 
     private Demo2Controller demo2Controller;
